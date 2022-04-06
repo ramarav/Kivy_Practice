85536fd958790961a134e8db05d5c25fcbdedeb8
ramarav <ramarav@gmail.com>
2022-04-06 16:05:04 +0530

    Box inside a box

---
+++ box3.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class Box3App(App):
+    pass
+class BoxLayoutExample(BoxLayout):
+    pass
+
+Box3App().run()
