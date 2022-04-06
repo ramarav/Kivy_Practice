f8b5fc2404f7e6e36157bef8e05894691ac6c92d
ramarav <ramarav@gmail.com>
2022-04-06 16:19:54 +0530

    Basic Anchor Layout

---
+++ anchor1.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class Anchor1App(App):
+    pass
+class BoxLayoutExample(BoxLayout):
+    pass
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+Anchor1App().run()
