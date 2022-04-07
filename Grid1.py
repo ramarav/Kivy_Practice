be8718a1b4d0209cd2ec868902688997b841bd78
ramarav <ramarav@gmail.com>
2022-04-07 19:02:09 +0530

    Basic Grid Layout

---
+++ Grid1.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class Grid1App(App):
+    pass
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+
+class GridLayoutExample(GridLayout):
+    pass
+
+
+Grid1App().run()
