39408b8b3bf008f9c3e807f8a57a88256e98e24c
ramarav <ramarav@gmail.com>
2022-04-07 19:11:20 +0530

    Grid Layout with box layout

---
+++ Grid2.py
@@ -0,0 +1,28 @@
+from kivy.app import App
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+
+class Grid2App(App):
+    pass
+
+
+class BoxLayoutExample(BoxLayout):
+    pass
+
+
+class AnchorLayoutExample(AnchorLayout):
+    pass
+
+
+class GridLayoutExample(GridLayout):
+    pass
+
+
+Grid2App().run()
